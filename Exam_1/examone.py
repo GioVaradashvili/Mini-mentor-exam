5b892b215377853a8b4965a4c37594b4a49412de
GioVaradashvili <153834276+GioVaradashvili@users.noreply.github.com>
2025-04-29 07:13:40 -0700

    1

--- Exam_1/examone.py
+++ Exam_1/examone.py
@@ -27,39 +27,12 @@ def delete_user():
             print("user deleted successfully!")
             
             return
-def balance():
-    value = input("Enter your balance: ")   
-    if value.isdigit():
-        print(f"Your balance is: {value}")
-    else:
-        print("Invalid input. Please enter a number.")
-
-def transfer():
-    amount = input("Enter the amount to transfer: ")
-    if amount.isdigit():
-        return "Transferring {amount}..."
-    else:
-        return "Invalid input. Please enter a number."
-
-def deposit():
-    amount = input("Enter the amount to deposit: ")
-    if amount.isdigit():
-        return "Depositing {amount}..."
-    else:
-        return "Invalid input. Please enter a number."
-    
-def output(
-    message):
-    print(message)
 
 while True:
         print("1. Add User")
         print("2. View Users")
         print("3. Delete User")
-        print("4. Check Balance")
-        print("5. Transfer Money")
-        print("6. Deposit Money")
-        print("7. Exit")
+        print("4. Exit")
         choice = input("Enter your choice: ")
         if choice == '1':
             add_user()
@@ -68,12 +41,6 @@ while True:
         elif choice == '3':
             delete_user()
         elif choice == '4':
-            balance("your balance is:")
-        elif choice == '5':
-            transfer("transfer is successfully!")
-        elif choice == '6':
-            deposit("deposit is successfully!")
-        elif choice == '7':
             print("Exiting...")
             break
         else:
